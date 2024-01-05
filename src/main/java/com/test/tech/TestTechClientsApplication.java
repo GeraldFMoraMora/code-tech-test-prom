adf22d58682424ff7ed2634aa4a5c21bc9843cc0
GeraldFMoraMora <geraldmoramora@gmail.com>
2024-01-05 12:47:03 -0600

    Agregada base de la aplicacion en springboot

---
+++ src/main/java/com/test/tech/TestTechClientsApplication.java
@@ -0,0 +1,14 @@
+package com.test.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechClientsApplication.class, args);
+		System.out.println("App is running...");
+	}
+
+}
