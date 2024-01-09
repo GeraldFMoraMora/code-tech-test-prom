469eb2ba1be00a9c16e3a81f81d1b59d9f4099a9
GeraldFMoraMora <geraldmoramora@gmail.com>
2024-01-08 23:08:14 -0600

    agregado Dockerfile

--- src/main/java/com/test/tech/TestTechClientsApplication.java
+++ src/main/java/com/test/tech/TestTechClientsApplication.java
@@ -2,9 +2,11 @@ package com.test.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.test.tech.model.entity")
 @EnableJpaRepositories(basePackages = "com.test.tech.model.dao")
 public class TestTechClientsApplication {
 
